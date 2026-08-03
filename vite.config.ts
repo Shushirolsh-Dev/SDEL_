1fd421a7dbfd456874279f3a6d7ed7db14b079c4
Jonathan Peter <126822560+Shushirolsh-Dev@users.noreply.github.com>
2026-08-03 07:24:13 +0100

    Add Tailwind CSS plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig(() => {
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-    server: {
-      hmr: true,
-    },
-  };
+  },
 });
