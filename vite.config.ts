e17974f8da8889cb2561796b4605f34d04338dc9
Jonathan Peter <126822560+Shushirolsh-Dev@users.noreply.github.com>
2026-09-14 17:58:37 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,43 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'Thesdel',
+        short_name: 'Thesdel',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#14151A',
+        theme_color: '#14151A',
+        description: 'Student timetable & attendance',
+
+        icons: [
+          {
+            src: '/converted-image.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/converted-image.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
